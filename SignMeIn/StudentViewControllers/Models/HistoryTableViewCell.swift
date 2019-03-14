b34a283bc99ac1f6755e11e961fadad08ffc1c24
gyul1 <gyulnarag01@gmail.com>
2019-03-13 22:28:18 -0700

    created history cell

---
+++ SignMeIn/StudentViewControllers/Models/HistoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryTableViewCell.swift
+//  SignMeIn
+//
+//  Created by Gyulnara Grigoryan  on 3/13/19.
+//  Copyright © 2019 ChenMo. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
